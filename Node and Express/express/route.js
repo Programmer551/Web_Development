6075b9ed7b9136084b70fe41554298272091b2f4
Programmer551 <m.duraid551@gmail.com>
2023-09-02 22:10:38 -0700

    Update route.js

--- Node and Express/express/route.js
+++ Node and Express/express/route.js
@@ -1,35 +1,55 @@
 const express = require("express");
 const app = express();
-const { data } = require(data);
-app.get("/", (req, res) => {
-  res.status(200).send(`<h1>Web Page</h1><a href="/api">API</a>`);
-});
-app.get("/api", (req, res) => {
-  const data2 = data.map((data) => {
-    const { Name, Class, Link } = data;
-    return { Name, Class, Link };
-  });
-  res.json(data2);
-});
-app.get("/api/:dataClass", (req, res) => {
-  // console.log(req.params)
+const data = [
+  {
+    id: 1,
+    name: "albany sofa",
+    image:
+      "https://dl.airtable.com/.attachments/6ac7f7b55d505057317534722e5a9f03/9183491e/product-3.jpg",
+    price: 39.95,
+    desc: `I'm baby direct trade farm-to-table hell of, YOLO readymade raw denim venmo whatever organic gluten-free kitsch schlitz irony af flexitarian.`,
+  },
+  {
+    id: 2,
+    name: "entertainment center",
+    image:
+      "https://dl.airtable.com/.attachments/da5e17fd71f50578d525dd5f596e407e/d5e88ac8/product-2.jpg",
+    price: 29.98,
+    desc: `I'm baby direct trade farm-to-table hell of, YOLO readymade raw denim venmo whatever organic gluten-free kitsch schlitz irony af flexitarian.`,
+  },
+  {
+    id: 3,
+    name: "albany sectional",
+    image:
+      "https://dl.airtable.com/.attachments/05ecddf7ac8d581ecc3f7922415e7907/a4242abc/product-1.jpeg",
+    price: 10.99,
+    desc: `I'm baby direct trade farm-to-table hell of, YOLO readymade raw denim venmo whatever organic gluten-free kitsch schlitz irony af flexitarian.`,
+  },
+  {
+    id: 4,
+    name: "leather sofa",
+    image:
+      "https://dl.airtable.com/.attachments/3245c726ee77d73702ba8c3310639727/f000842b/product-5.jpg",
+    price: 9.99,
+    desc: `I'm baby direct trade farm-to-table hell of, YOLO readymade raw denim venmo whatever organic gluten-free kitsch schlitz irony af flexitarian.`,
+  },
+];
+const people = [
+  { id: 1, name: "john" },
+  { id: 2, name: "peter" },
+  { id: 3, name: "susan" },
+  { id: 4, name: "anna" },
+  { id: 5, name: "emma" },
+];
 
-  const { dataClass } = req.params;
-  console.log(dataClass);
 
-  var data3 = data.find((data) => data.Class === dataClass);
+app.get("/api/:ProductId", () => {
+  const { ProductId } = req.params;
+  const data2=data.find(()=>data.id===Number(ProductId))
   if (!data3) {
-    return res.status(404).send("Page not Found");
-  }
-  return res.status(200).json(data3);
+    return res.status(404).send("Page not Found")
+  } 
+    return res.status(200).json(data3);
 });
-// app.get("/api/2", (req, res) => {
-//   const data3 = data.find((data) => data.Class==="Eight");
-//   res.json(data3);
-// });
-app.get("*", (req, res) => {
-  res.status(404).send("Page not Found");
-});
-app.listen(3000, () => {
-  console.log("App is listening");
-});
+
+app.listen(3000);
