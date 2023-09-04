f8c2712e4c9bb9e7b1cbe459d5a793c4508623e2
Programmer551 <m.duraid551@gmail.com>
2023-09-04 04:41:31 -0700

    Create middleware.js

---
+++ Node and Express/express/middleware.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const looger = (req, res, next) => {
+  console.log("logger is running");
+  next();
+};
+app.get("/", looger, (req, res) => {
+  res.send("Home");
+});
+
+app.get("/about", looger, (req, res) => {
+  res.send("About");
+});
+app.listen(3000, () => {
+  console.log("App is running");
+});
