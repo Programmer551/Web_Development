e352ace56ca2d2dae0ef5769ff2b9f41d5884a8c
Programmer551 <m.duraid551@gmail.com>
2023-09-09 08:18:35 +0500

    Update post.js

--- Node and Express/express/post.js
+++ Node and Express/express/post.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const { people } = require("./data");
-app.use(express.static("methods-public"));
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 // app.get("/people", (req, res) => {
 //   res.status(200).json({ data: people });
@@ -28,21 +28,38 @@ app.post("/login", (req, res) => {
 app.get("/api/people", (req, res) => {
   res.send("Hello");
 });
-app.post("/api/people", (req, res) => {
+// app.post("/api/people", (req, res) => {
+//   const { name } = req.body;
+//   if (!name) {
+//     return res.status(400).json({ sucess: false });
+//   }
+//   res.status(200).send({ sucess: true, person: name });
+//   // res.json(name);
+// });
+// app.post("api/postman/people", (req, res) => {
+//   const { name } = req.body;
+//   if (!name) {
+//     return res.status(400).json({ sucess: false });
+//   }
+//   res.status(200).send({ sucess: true, person: [...people] });
+//   // res.json(name);
+// });
+app.put("/login/:id", (req, res) => {
+  const { id } = req.params;
   const { name } = req.body;
-  if (!name) {
-    return res.status(400).json({ sucess: false });
-  }
-  res.status(200).send({ sucess: true, person: name });
-  // res.json(name);
-});
-app.post("api/postman/people", (req, res) => {
-  const { name } = req.body;
-  if (!name) {
-    return res.status(400).json({ sucess: false });
-  }
-  res.status(200).send({ sucess: true, person: [...people] });
-  // res.json(name);
+  console.log(id, name);
+  // console.log("Hello World");
+  // res.send("Hello World").status(200);
+  const Person = people.find((people) => people.id === Number(id));
+  // res.send(Person);
+  const newPeople = people.map((Person) => {
+    if (Person.id === Number(id)) {
+      Person.name = "Duraid";
+    }
+    return Person;
+  });
+  res.send(newPeople);
+  console.log(newPeople);
 });
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
