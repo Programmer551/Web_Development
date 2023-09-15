2ade95d6de5f1817b60721de9822b4b070389add
Programmer551 <m.duraid551@gmail.com>
2023-09-15 16:31:37 +0500

    Web

---
+++ Mongo DB/mongo1.js
@@ -0,0 +1,9 @@
+// These are starter commands
+// show dbs
+// use "databse name"
+//db."collection name"."document name".insertOne({object})
+//db."collection name"."document name".insertMany([{many objects},{many objects}])
+//db."collection name".find({},{})
+// If we want to add multiple documents fastly we can use the array technique for compass
+//db."collection name".find().sort({}) One for assending order and -1 for dessending order
+//db."collection name".find().limit()
