3fba8243c5902c4f38a109ac519bd8022452288b
Duraid <m.duraid551@gmail.com>
2024-03-09 08:17:59 +0500

    Update

--- Three Js/three/src/App.jsx
+++ Three Js/three/src/App.jsx
@@ -1,13 +1,49 @@
+import { useRef } from "react";
 import "./App.css";
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useFrame } from "@react-three/fiber";
+const Cube = ({ position, size, color }) => {
+  const ref = useRef();
+  useFrame((state, delta) => {
+    ref.current.rotation.x += delta;
+    ref.current.rotation.y += delta;
+    // ref.current.rotation.z += delta;
+    ref.current.position.z = Math.cos(state.clock.elapsedTime) * 5;
+  });
+  return (
+    <mesh
+      position={position}
+      ref={ref}>
+      <boxGeometry args={size} />
+      <meshStandardMaterial color={color} />
+    </mesh>
+  );
+};
 function App() {
   return (
     <Canvas>
-      <directionalLight position={[2, 2, 5]} />
-      <mesh>
-        <boxGeometry args={[2, 2, 2]} />
-        <meshStandardMaterial color={"red"} />
-      </mesh>
+      <directionalLight position={[0, 0, 2]} />
+      <ambientLight />
+
+      <Cube
+        position={[-1, 0, 0]}
+        color='red'
+        size={[1, 1, 1]}
+      />
+      <Cube
+        position={[1, 0, 0]}
+        color='green'
+        size={[1, 1, 1]}
+      />
+      <Cube
+        position={[-1, 2, 0]}
+        color='blue'
+        size={[1, 1, 1]}
+      />
+      <Cube
+        position={[1, 2, 0]}
+        color='orange'
+        size={[1, 1, 1]}
+      />
     </Canvas>
   );
 }
